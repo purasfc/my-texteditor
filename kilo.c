8a3614b473f9538154ae9ea35238bf2de96781ee
purasfc <pura@sfc.wide.ad.jp>
2026-03-16 16:32:33 +0100

    iscntrl()

--- kilo.c
+++ kilo.c
@@ -1,3 +1,6 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
@@ -12,7 +15,7 @@ void enableRawMode(void) {
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO);
+	raw.c_lflag &= ~(ECHO | ICANON);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -21,6 +24,13 @@ int main(void) {
 	enableRawMode();
 
 	char c;
-	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		}
+		else {
+			printf("%d ('%c')\n", c, c);
+		}
+	}
 	return 0;
 }

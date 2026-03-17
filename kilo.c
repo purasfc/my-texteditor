d6e65fb8740c38bebea21015928d7d7f1d9abb13
purasfc <pura@sfc.wide.ad.jp>
2026-03-17 14:00:43 +0100

    gloabl state

--- kilo.c
+++ kilo.c
@@ -11,7 +11,11 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
-struct termios orig_termios;
+struct editorConfig {
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -23,15 +27,15 @@ void die(const char *s) {
 }
 
 void disableRawMode(void) {
-		if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+		if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 			die("tcsetattr");
 }
 
 void enableRawMode(void) {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
@@ -53,10 +57,20 @@ char editorReadKey(void) {
 }
 
 /*** output ***/
+
+void editorDrawRows(void) {
+	int y;
+	for (y=0; y<24; y++) {
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
  
 void editorRefreshScrean(void) {
 	write(STDOUT_FILENO, "\x1b[2J", 4); /* 4 -> writing four bytes. \x1b -> escape character, or 27 in decimal. = writing escape sequence to the terminal. escape sequence always start from an escape character (27) followed by [ character. J -> Erase In Dispay. Escape sequence cmmands take arguments, which come before the command. In this case, the argument is 2, -> clear the <screen>. <esc>[1J would clear the screen up to where the cursor is, and <esc>[0J */
 	write(STDOUT_FILENO, "\x1b[H", 3); /* reposition to the top of the terminal */
+	
+	editorDrawRows();
+	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/
